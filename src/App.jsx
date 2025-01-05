c0b6db944ab4f5ecc9645762805f291975fd0d07
Myagmardorj <borisinparis@Myagmardorjs-MacBook-Pro.local>
2025-01-05 19:38:30 +0800

    second

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,39 @@ import './App.css'
 function App() {
   const [tasks, setTasks] = useState([]);
   const [newTask, setNewTask] = useState({})
-
   const [liked, setLiked] = useState(true);
-  
-
   function hanleInputChange(event) {
 
 
     setNewTask({isActive: true, description: event.target.value})
+    console.log(newTask);
+    
   }
-  function addTask() {if(!newTask) return;
+
+
+  function addTask() {
+    
+    if(!newTask)
+      return;
     setTasks([...tasks, newTask]);
     console.log(tasks);
     
+    
   }
   function filterTodo (text){
+    if(text === "all") {
+      setTasks(tasks)
+      console.log("all daragdlaa");
+      
+    }
+
+
     if(text == "active"){
 
       const filteredTask = tasks.filter((el)=> {return el.isActive === true})
       setTasks(filteredTask)
+      console.log("active daragdlaa");
+      
     } else if (text == "completed"){
 
       const filteredTask = tasks.filter((el)=> {return el.isActive === false})
@@ -38,10 +52,14 @@ function App() {
 
   }
 
-
-  const onchangeCheckBox = () => {
+  const clearbottom= () => {
+    alert("cleared")
 
   }
+  const onchangeCheckBox = (e) => {
+
+    setLiked(e.target.checked)}
+
 
   return (
     <>
@@ -50,14 +68,14 @@ function App() {
         <div className='addflex'>
           <input onChange={hanleInputChange} className='inputer' type='text' placeholder='add a task...'
           />
-          <button className='add-button'
+          <button type='submit' className='add-button'
             onClick={addTask}
           >
             add
           </button>
         </div>
         <div className='tab-main'>
-          <button className='tab'>all</button>
+          <button onClick={()=>filterTodo("all")} className='tab-all'>all</button>
           <button onClick={()=>filterTodo("active")} checked={liked} className='tab'>
            active
           </button>
@@ -65,11 +83,21 @@ function App() {
         </div>
         {
           tasks.length === 0 ? <p>no tasks yet . add one above</p> : tasks.map((task, index) =>
-            <div style={{display: "flex", gap: "10px"}} key={index}>
-              <input type='checkbox' onChange={onchangeCheckBox()}/>
-              <p className='taskshow'>{task.description}</p>
-            </div>
+            <button className='todolists' key={index} checked={liked} onClick={onchangeCheckBox} >
+              <input type='checkbox'/>
+              <p className='taskshow'>{liked ? <del>{task.description}</del> : task.description }</p>
+              <button className='deleter'>Delete</button>
+            </button>
           )
+        }
+        {
+          tasks.length === 0 ? <p></p>: <div className='lineBottom'></div> 
+        }
+        {
+          tasks.length === 0 ? <div></div> : <div className='countertask'>
+            <div className='duusahh'> of tasks compeleted </div> 
+            <div className='duusah' onClick={clearbottom}>clear completed</div>
+          </div>
         }
         <div className='footer container'>Powered by Pinecone academy</div>
       </div>
