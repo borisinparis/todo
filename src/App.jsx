2156d36762f582f53c5277150cd7eff6ac440598
borisinparis <mnkymgaa@gmail>
2025-01-06 20:23:00 +0800

    duusan

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,57 @@
 import { useState } from 'react'
 import './App.css'
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
+
   const [tasks, setTasks] = useState([]);
-  const [newTask, setNewTask] = useState({})
-  const [liked, setLiked] = useState(true);
-  function hanleInputChange(event) {
 
+  const [newTask, setNewTask] = useState("");
 
-    setNewTask({isActive: true, description: event.target.value})
-    console.log(newTask);
-    
+  const [error, setError] = useState(false);
+
+  const [filterState, setFilterState] = useState("all")
+
+  const hanleInputChange = (event) => {
+    setNewTask(event.target.value);
   }
-
-
-  function addTask() {
-    
-    if(!newTask)
-      return;
-    setTasks([...tasks, newTask]);
-    console.log(tasks);
-    
-    
-  }
-  function filterTodo (text){
-    if(text === "all") {
-      setTasks(tasks)
-      console.log("all daragdlaa");
-      
+  const addTask=() => {
+    if (newTask.length === 0) {
+      setError(true);
+      alert("ta yum bichnuuu")
     }
-
-
-    if(text == "active"){
-
-      const filteredTask = tasks.filter((el)=> {return el.isActive === true})
-      setTasks(filteredTask)
-      console.log("active daragdlaa");
-      
-    } else if (text == "completed"){
-
-      const filteredTask = tasks.filter((el)=> {return el.isActive === false})
-      setTasks(filteredTask)
-      console.log("completed daragdlaa");
-      
-    } else {
-      return;
+    else {
+      setError(false);
+      setTasks([...tasks, {
+        description:newTask, status: "active", id: uuidv4()
+      }]);
+      setNewTask("")
     }
-
-
-
-
+  }
+  
+  const clearbottom = () => {
+    const clearCompleted = tasks.filter((task) => task.status !== "completed")
+    setTasks(clearCompleted)
+  }
+  const deletebottom =(id) => {
+    const deleted = tasks.filter((task) => task.id !==id )
+    setTasks(deleted)
   }
 
-  const clearbottom= () => {
-    alert("cleared")
-
+  const handleFilterStateChange = (state) => {
+    setFilterState(state);
   }
-  const onchangeCheckBox = (e) => {
-
-    setLiked(e.target.checked)}
-
-
+  const onChangeCheckBox = (id) => {
+    const taskOf = tasks.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, status: todo.status === "active" ? "completed" : "active" }
+      }
+      else {
+        return todo
+      }
+    })
+    setTasks(taskOf)
+  }
   return (
     <>
       <div className='main-contain'>
@@ -68,34 +59,41 @@ function App() {
         <div className='addflex'>
           <input onChange={hanleInputChange} className='inputer' type='text' placeholder='add a task...'
           />
-          <button type='submit' className='add-button'
-            onClick={addTask}
-          >
+          <button type='submit' className='add-button' onClick={addTask}>
             add
           </button>
         </div>
         <div className='tab-main'>
-          <button onClick={()=>filterTodo("all")} className='tab-all'>all</button>
-          <button onClick={()=>filterTodo("active")} checked={liked} className='tab'>
-           active
-          </button>
-          <button  onClick={()=>filterTodo("completed")} className='tab'>completed</button>
+          <button onClick={() => handleFilterStateChange("all")} className='tab'>all</button>
+          <button onClick={() => handleFilterStateChange("active")} className='tab'>active</button>
+          <button onClick={() => handleFilterStateChange("completed")} className='tab'>completed</button>
         </div>
         {
-          tasks.length === 0 ? <p>no tasks yet . add one above</p> : tasks.map((task, index) =>
-            <button className='todolists' key={index} checked={liked} onClick={onchangeCheckBox} >
-              <input type='checkbox'/>
-              <p className='taskshow'>{liked ? <del>{task.description}</del> : task.description }</p>
-              <button className='deleter'>Delete</button>
+          tasks.length === 0 ? <p>no tasks yet . add one above</p> :
+           tasks.filter((todo) => {
+            if (filterState === "active") {
+              return todo.status === "active";
+
+            } else if (filterState === "completed") {
+              return todo.status === "completed"
+            }
+            else {
+              return true
+            }
+          }).map((todo) => {
+            return <button className='todolists' >
+              <input type="checkbox" checked={todo.status === "completed"} onChange={() => onChangeCheckBox(todo.id)} />
+              <p className='taskshow'>{todo.status==="completed" ? <del>{todo.description}</del> : todo.description}</p>
+              <button onClick={()=>{deletebottom(todo.id)}} className='deleter'>Delete</button>
             </button>
-          )
+ } )
         }
         {
-          tasks.length === 0 ? <p></p>: <div className='lineBottom'></div> 
+          tasks.length === 0 ? <p></p> : <div className='lineBottom'></div>
         }
         {
           tasks.length === 0 ? <div></div> : <div className='countertask'>
-            <div className='duusahh'> of tasks compeleted </div> 
+            <div className='duusahh'> {tasks.filter(task => task.status === 'completed').length} of {tasks.length} tasks completed</div>
             <div className='duusah' onClick={clearbottom}>clear completed</div>
           </div>
         }
